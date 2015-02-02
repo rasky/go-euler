78f59f8c4a4f8e5224aed75990c435e1ed05936f
Giovanni Bajo <rasky@develer.com>
2015-02-02 23:06:02 +0100

    Implement ContinuedFraction()

--- natural/rational.go
+++ natural/rational.go
@@ -2,6 +2,7 @@ package natural
 
 import (
 	"fmt"
+	"math/big"
 
 	"code.google.com/p/intmath/i64"
 )
@@ -23,9 +24,20 @@ func LCM(a, b int64) int64 {
 	return a * b / GCD(a, b)
 }
 
+func (r1 Rational) Reciprocal() Rational {
+	return Rational{r1.Den, r1.Num}
+}
+
 func (r1 Rational) Equal(r2 Rational) bool {
 	lcm := LCM(r1.Den, r2.Den)
 	return r1.Num*(lcm/r1.Den) == r2.Num*(lcm/r2.Den)
+}
+
+func (r1 Rational) Sum(r2 Rational) Rational {
+	lcm := LCM(r1.Den, r2.Den)
+	num1 := r1.Num * (lcm / r1.Den)
+	num2 := r2.Num * (lcm / r2.Den)
+	return Rational{num1 + num2, lcm}
 }
 
 func (r1 Rational) Reduce() Rational {
@@ -47,4 +59,15 @@ func (r1 Rational) Mul(r2 Rational) Rational {
 
 func (r Rational) String() string {
 	return fmt.Sprintf("<%d/%d>", r.Num, r.Den)
+}
+
+func ContinuedFraction(terms []int64) *big.Rat {
+
+	n := len(terms)
+	r := big.NewRat(terms[n-1], 1)
+	for i := n - 2; i >= 0; i-- {
+		r.Inv(r)
+		r.Add(r, big.NewRat(terms[i], 1))
+	}
+	return r
 }
