747c0abd4adf5f321dda74081f9e422486b3c03a
Giovanni Bajo <rasky@develer.com>
2015-02-02 22:09:07 +0100

    New problem

---
+++ p55_test.go
@@ -0,0 +1,20 @@
+package euler
+
+import (
+	"euler/natural"
+	"testing"
+)
+
+func TestP55(t *testing.T) {
+
+	count := 0
+	for i := int64(1); i < 10000; i++ {
+		if natural.IsLychrel(i, 50) {
+			count++
+		}
+	}
+
+	if count != 249 {
+		t.Error(count)
+	}
+}
