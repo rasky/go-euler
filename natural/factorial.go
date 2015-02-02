4e795c405910974f5ef30c33ab272bde2432197d
Giovanni Bajo <rasky@develer.com>
2015-02-02 20:39:13 +0100

    Add FactoralArray

--- natural/factorial.go
+++ natural/factorial.go
@@ -9,3 +9,12 @@ func Factorial(n int) *big.Int {
 	}
 	return f
 }
+
+func FactorialArray(n int64) []*big.Int {
+	fact := make([]*big.Int, n)
+	fact[0] = big.NewInt(1)
+	for i := int64(1); i < n; i++ {
+		fact[i] = big.NewInt(0).Mul(fact[i-1], big.NewInt(i))
+	}
+	return fact
+}
